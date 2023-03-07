935e56794110515c4e77966b82861fe2868bc48c
lucasdemel0 <122926349+lucasdemel0@users.noreply.github.com>
2023-03-07 16:11:41 -0500

    Add files via upload

--- encoder.py
+++ encoder.py
@@ -1,46 +1,33 @@
-
 #Lucas de Melo
+
+def encode(password):
+    encoded = ""
+    for i in password:
+        digit = int(i) + 3
+        encoded += str(digit)
+    return encoded
 
 
 def main():
-    check = True
-    while check:
-        menu()
-        option = int(input("Please enter an option: "))
-        if option == 1:
+
+    encoded = None
+
+    while True:
+        print("Menu")
+        print("-------------")
+        print("1. Encode")
+        print("2. Decode")
+        print("3. Quit")
+        print()
+
+        menu_selection = int(input("Please enter an option: "))
+        if menu_selection == 1:
             password = input("Please enter your password to encode: ")
             encoded = encode(password)
-            print("Your password has been encoded and stored!")
-        elif option==2:
-            print("The encoded password is "+encoded+", and the original "
-                                                      "password is " +password)
-        elif option==3:
-            check = False
-        else:
-            print("Enter an option from 1-3")
 
-def encode(password):
-    arr = []
-    for val in password:
-        if int(val) == 7:
-            arr.append(0)
-        elif int(val) == 8:
-            arr.append(1)
-        elif int(val) == 9:
-            arr.append(2)
-        else:
-            arr.append(int(val)+3)
 
-    encoded = ""
-    for val in arr:
-        encoded += str(val)
-    return encoded
-
-def menu():
-    print("Menu\n-------------\n1. Encode\n2. Decode\n3. Quit")
+        elif menu_selection == 3:
+            break
 
 if __name__ == "__main__":
     main()
-
-
-
